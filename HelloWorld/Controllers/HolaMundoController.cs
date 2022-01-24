0db12a875b77cd52de6bddc9aaa341dd3516598e
Javier Blanco <jadavid24@hotmail.com>
2022-01-24 15:25:32 -0500

    No se

---
+++ HelloWorld/Controllers/HolaMundoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HolaMundoController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetHolaMundo()
+    {
+        return Ok("Hola Mundo");
+    }
+}
